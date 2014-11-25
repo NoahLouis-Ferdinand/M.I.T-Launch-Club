0ef251aac3782695201496de84d3c50a1452ffdf
tbh11 <alex@raistrick.net>
2014-11-24 22:00:43 -0500

    Commit v0.0.1
    -Mouse Look Script
    -First Person Controller Script
    -Player Pre-Fab

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	public float horizontalMouseSensitivity = 5;
+	public float verticalMouseSensitivity = 7;
+	public float mouseAngleLimit = 80;
+	private float pitch = 0;
+	Camera camera;
+	// Use this for initialization
+	void Start () {
+		camera = GetComponentInChildren<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(0,Input.GetAxis("Mouse X")*horizontalMouseSensitivity,0);
+		pitch -= Input.GetAxis("Mouse Y") * verticalMouseSensitivity;
+		pitch = Mathf.Clamp(pitch, -mouseAngleLimit, mouseAngleLimit);
+		camera.transform.localRotation = Quaternion.Euler(pitch, 0, 0);
+	}
+}
