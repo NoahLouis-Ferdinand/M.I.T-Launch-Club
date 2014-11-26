753a7930233402a9086c68d5375c779acefdd5d3
tbh11 <alex@raistrick.net>
2014-11-25 21:18:03 -0500

    Commit v0.0.2
    Made a fire projectile, a bunch of scripts that make it work, and added
    temporary code into the player prefab (An instantiate code to create
    projectiles) to fire the spell in the facing of the camera, with offsets
    and rotations

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Player Scripts/FirstPersonController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FirstPersonController : MonoBehaviour {
+
+	CharacterController controller;
+	Camera camera;
+	public float speedMultiplier = 5;
+	public float terminalVelocity = 120;
+	public float jumpImpulse = 3;
+	public float standingHeight = 2;
+	public float crouchedHeight = 1;
+	private Vector3 velocity = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+		camera = GetComponentInChildren<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+		// Movement
+		velocity.y += Physics.gravity.y * Time.deltaTime;
+		if( controller.isGrounded) {
+			if(Input.GetButton("Jump")){velocity.y = jumpImpulse;}
+			if(Input.GetButtonDown("Crouch")){
+				Crouch(crouchedHeight);
+			}
+		}else{
+			velocity.y = Mathf.Clamp(velocity.y, -terminalVelocity, terminalVelocity);
+		}
+
+		if(Input.GetButtonUp("Crouch")){
+			controller.SimpleMove (new Vector3(0,20,0));
+			Crouch(standingHeight);
+		}
+
+		velocity.x = Input.GetAxis("Horizontal")*speedMultiplier;
+		velocity.z = Input.GetAxis("Vertical")*speedMultiplier;
+
+		velocity = transform.rotation * velocity;
+		controller.Move(velocity * Time.deltaTime);
+
+	}
+
+	private void Crouch(float height){
+		controller.height = height;
+		camera.transform.position.Set(0,height,0);
+
+	}
+}
+
+
+
---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/TimedLife.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimedLife : MonoBehaviour {
+
+	// Use this for initialization
+	public float lifeSpan;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeSpan -= Time.deltaTime;
+		if(lifeSpan<=0){
+				Destroy(gameObject);
+		}
+	}
+}
