7d093e651a1bc2c6afcc2920b0daf80bccd01c5c
tbh11 <alex@raistrick.net>
2014-11-27 11:15:56 -0500

    Commit v0.0.3
    Changed from a hard coded projectile summoning system to a more modular
    ICastable interface and Caster script. Also added in a HasHealth script,
    but no ways to take damage as of yet. IKillable was also added

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/OnImpactScripts/InstantiateOnImpact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiateOnImpact : MonoBehaviour {
+
+	public GameObject effect;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Instantiate (effect, transform.position, Quaternion.identity);
+	}
+}
---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Spell Scripts/ProjectileSpell.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileSpell : MonoBehaviour, ICastable {
+
+	// Use this for initialization
+
+	public GameObject projectile;
+	public float baseManaCost;
+
+	public void Cast(float magnitude, Vector3 position, Vector3 offset, Quaternion rotation){
+		Instantiate (projectile,position+(rotation*offset),rotation);
+	} 
+
+	public float GetBaseManaCost(){
+		return baseManaCost;
+	}
+}
