22379d1210f3c65c19430e8f26195a76c52da5b0
tbh11 <alex@raistrick.net>
2014-11-28 08:19:30 -0500

    Commit v0.0.5
    Added PlayerHealth and PlayerDestroy. Added a secondary camera to the
    devscene, for future use in a respawn script

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Destruction Scripts/DestroyOnParticleFinish.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyOnParticleFinish : MonoBehaviour {
+
+	private IDestroyable destroy;
+	private ParticleSystem particle;
+	private float lifeSpan;
+	public float durationMultiplier = 1;
+	
+	void Start () {
+		destroy = (IDestroyable) GetComponent (typeof(IDestroyable));
+		particle = (ParticleSystem)GetComponentInChildren<ParticleSystem> ();
+		lifeSpan = particle.duration*durationMultiplier;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lifeSpan -= Time.deltaTime;
+		if(lifeSpan<=0){
+			destroy.Destroy();
+		}
+	}
+}
---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Player Specific Scripts/PlayerDeathHandler.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeathHandler : MonoBehaviour, IDestroyable {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public void Destroy(){
+		Destroy (gameObject);
+	}
+}
