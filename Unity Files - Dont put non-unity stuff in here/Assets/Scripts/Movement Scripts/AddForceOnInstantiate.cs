e2a3c5e4e83780cee43979bf5c262f88b0d5ec7f
tbh11 <alex@raistrick.net>
2014-12-04 22:50:22 -0500

    Commit v0.0.7
    Very small changes, did a bit work on the slime thing

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Movement Scripts/AddForceOnInstantiate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class AddForceOnInstantiate: MonoBehaviour {
+	
+	// Use this for initialization
+	private Rigidbody body;
+	public float force;
+	
+	void Start () {
+		body = GetComponent<Rigidbody> ();
+		//body.useGravity = false;
+		body.AddForce (transform.forward * force);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	
+}
