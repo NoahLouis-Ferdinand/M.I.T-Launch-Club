9eb694cbd0c1c1e7a312faf75d6dfad40c5ddcae
Alex <alex@raistrick.net>
2014-12-05 10:48:13 -0500

    Commit v0.0.8
    Broke the caster system while implementing a multiple spell, selection
    based system, il finish it for the next commit probably

--- Unity Files - Dont put non-unity stuff in here/Assets/Scripts/DestroyHandlers/InstantiateOnDestroy.cs
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/DestroyHandlers/InstantiateOnDestroy.cs
@@ -6,7 +6,9 @@ public class InstantiateOnDestroy : MonoBehaviour, IDestroyable {
 	public GameObject objectToInstantiate;
 
 	public void Destroy(){
-		Instantiate (objectToInstantiate,transform.position, transform.rotation);
+		if (objectToInstantiate != null) {
+			Instantiate (objectToInstantiate,transform.position, transform.rotation);
+		}
 		Destroy (gameObject);
 	}
 }
--- Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Player Specific Scripts/Caster.cs
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Player Specific Scripts/Caster.cs
@@ -4,19 +4,24 @@ using System.Collections;
 public class Caster : MonoBehaviour {
 
 	// Use this for initialization
-	private ICastable spell;
+	private ICastable[] spells;
 	public Vector3 offset;
 	private Camera view;
+	public int selection = 0;
 
 	void Start () {
-		spell = (ICastable) GetComponent(typeof(ICastable));
+		spells = (ICastable[]) GetComponents(typeof(ICastable));
 		view = (Camera) GetComponentInChildren<Camera> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("Fire1")){
-			spell.Cast(1, transform.position, offset, view.transform.rotation);
+			if(spells[selection]!=null){
+				spells[selection].Cast(1, transform.position, offset, view.transform.rotation);
+			}else{
+				Debug.Log ("Failed attempting to cast spell at selection " + selection.ToString() + " because it was null!");
+			}
 		}
 	}
 }
