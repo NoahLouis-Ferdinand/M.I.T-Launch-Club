f27d15d42705784aa13ff298aa7a5f739e8c1c73
Alex <alex@raistrick.net>
2015-02-21 20:51:40 -0500

    Commit v0.1.1
    I did some more work on the animator, made it more smooth, and more FPS
    like. Also started on a first person camera script with animations.
    
    None of this is done yet, im just committing to move all my work over to
    my desktop

---
+++ Unity Files - Dont put non-unity stuff in here/Assets/Scripts/Navigation/NavigateToObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NavigateToObject : MonoBehaviour {
+
+	// Use this for initialization
+	public string objectName;
+	private Transform t;
+	private NavMeshAgent nav;
+
+	void Start () {
+		t = GameObject.Find (objectName).transform;
+		nav = GetComponent<NavMeshAgent> ();
+		nav.SetDestination (t.position);
+		Debug.Log (t.position);
+	}
+}
